237d0d0b97d549fdb050bbada86c54af800186b0
Tuan <atuanle@hotmail.com>
2023-06-02 22:35:08 -0700

    Tuan - Activity #7

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import admin
 from django.urls import path, include  # <-- Make sure you have both of these imports.
 
@@ -20,4 +21,6 @@ urlpatterns = [
     path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
     path('', include('blogging.urls')),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
